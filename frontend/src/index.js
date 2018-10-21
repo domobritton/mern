6d37ff9edc88ed240fc6d9023700072ec8065a6e
domobritton <domonicbritton@gmail.com>
2018-10-21 10:39:17 -0700

    Add react frontend user session redux

---
+++ frontend/src/index.js
@@ -0,0 +1,28 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import App from './App';
+import jwt_decode from 'jwt-decode';
+import * as APIUtil from './util/session_api_util';
+import configureStore from './store/store';
+import registerServiceWorker from './registerServiceWorker';
+
+document.addEventListener('DOMContentLoaded', () => {
+    let store = configureStore();
+
+    if (localStorage.jwtToken) {
+        APIUtil.setAuthToken(localStorage.jwtToken);
+        const decoded = jwt_decode(localStorage.jwtToken);
+        store.dispatch(APIUtil.setCurrentUser(decoded));
+
+        const currentTime = Date.now() / 1000;
+        if (decoded.exp < currentTime) {
+            store.dispatch(APIUtil.logoutUser());
+            window.location.href = '/login';
+        }
+    }
+
+    const root = document.getElementById('root');
+    ReactDOM.render(<App store={store} />, root);
+    registerServiceWorker();
+});
